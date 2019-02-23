77aab0b592ae0224a112cf53baf983ff3bc3e80c
sm1987 <smukadam1987@gmail.com>
2019-02-23 11:15:24 +0530

    Final commit

---
+++ song.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var favouriteSong = "Love the way you lie";
+var released = "June 25, 2010";
+var studio = {
+    1 : "Effigy",
+    2 : "Fernade",
+    3 : "Michigan Sun",
+    4 : "Dublin",
+    5 : "Ireland"
+}
+var genre = "Hip Hop";
+var length = 4.23;
+var label = {
+    1 : "Aftermath",
+    2 : "Shady",
+    3 : "Interscope"
+}
+var songWriters = ["Eminem", "Alex da Kid", "Skylar Grey"];
+var producers = [songWriters[1], "Makeba Riddick"];
+
+console.log(favouriteSong);
+console.log(released);
+console.log(studio);
+console.log(genre);
+console.log(length);
+console.log(label);
+console.log(songWriters);
+console.log(producers);
